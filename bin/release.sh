a1c47b2b74f6c96149d717be90c07a1b273ced69
attila.lendvai <attila.lendvai@gmail.com>
2018-02-07 13:05:19 +0000

    prepare for release 1.5
    Ignore-this: 417350676e91e6f6d7f41f64a7ad0b7e
    
    darcs-hash:987ecc38fa3a940db9285c036a1f11fdf64771d1

--- bin/release.sh
+++ bin/release.sh
@@ -11,14 +11,13 @@ if [ -e "$ARCHIVE_FULLNAME" ]; then
   exit -1
 fi
 
-echo "Don't forget to tag the darcs repo! Creating release $ARCHIVE_FULLNAME, press enter to continue..."
-read
+echo "\nCreating release '$VERSION'\n\nDon't forget to tag the darcs repo!\nOutput will be $ARCHIVE_FULLNAME.\n\nPress enter to continue..."
+read tmp
 cd /project/iterate/darcs
 cp -r iterate iterate-$VERSION
-tar --exclude="iterate*/_darcs*" -zvcf "$ARCHIVE_FULLNAME" iterate-$VERSION
+tar --exclude="iterate*/_darcs*" --exclude="iterate*/.*" --exclude="iterate*/bin" -zvcf "$ARCHIVE_FULLNAME" iterate-$VERSION
 rm -rf iterate-$VERSION
 cd "$ARCHIVE_PATH"
-gpg -b -a "$ARCHIVE_NAME"
+gpg --detach-sign --armor "$ARCHIVE_NAME"
 ln -fs "$ARCHIVE_NAME" iterate-current.tar.gz
 ln -fs "$ARCHIVE_NAME".asc iterate-current.tar.gz.asc
-
